afa66273a9010ae0626cb30f6b1aedfe67fe9f1e
Tope-stack <yusufftope70@gmail.com>
2023-02-28 09:19:37 +0100

    Corrected nullable error

--- Models/Domain/Expense.cs
+++ Models/Domain/Expense.cs
@@ -4,7 +4,7 @@
     {
         public Guid Id { get; set; }
         public DateTime ExpenseDate { get; set; }
-        public string? MerchantName { get; set; }
+        public string MerchantName { get; set; }
         public int Amount { get; set; }
         public Classification Category { get; set; }
     }
